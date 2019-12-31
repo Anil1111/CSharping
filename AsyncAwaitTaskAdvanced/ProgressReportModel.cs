13548c042020d4bc3981a97d19de13f59e4b915f
Kanna Reddy <inbox@kannareddy.com>
2019-12-30 19:25:22 -0500

    Advanced Async Await Task Project has been added

---
+++ AsyncAwaitTaskAdvanced/ProgressReportModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncAwaitTaskAdvanced
+{
+    class ProgressReportModel
+    {
+        public int PercentageComplete { get; set; } = 0;
+        public List<WebsiteDataModel> SitesDownloaded { get; set; } = new List<WebsiteDataModel>();
+    }
+}
---
+++ AsyncAwaitTaskAdvanced/WebsiteDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncAwaitTaskAdvanced
+{
+    class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
